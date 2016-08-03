9c6be2f6f3e3acfa997ec6b1067980bc5b41415c
刘 珂 <liulk@cn.ibm.com>
2016-08-03 13:54:55 +0800

    update

--- WatsonMobileUI/DetailViewController.swift
+++ WatsonMobileUI/DetailViewController.swift
@@ -15,9 +15,9 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let data = Goods.init(id: "15", name: "CITIZEN TITANIA LINE HAPPY FLIGHT", price: "¥575,000", details: "Citizen Watch Xc Happy Flight Titania Line Eco-drive Eco-drive Type Display Type Needle Receiving Station Multi-radio Clock Kitagawa Keiko Ad Wearing Model Media Model Ec1044-55w Women", imgurl: "http://fs.scene7.com/is/image/flagshop/v_243815_01.jpg?$500$")
-        */
+        
+        //let data = Goods.init(id: "15", name: "CITIZEN TITANIA LINE HAPPY FLIGHT", price: "¥575,000", details: "Citizen Watch Xc Happy Flight Titania Line Eco-drive Eco-drive Type Display Type Needle Receiving Station Multi-radio Clock Kitagawa Keiko Ad Wearing Model Media Model Ec1044-55w Women", imgurl: "http://fs.scene7.com/is/image/flagshop/v_243815_01.jpg?$500$")
+        
         print("detailviewcontroller load.")
         
         let screenWidth = UIScreen.mainScreen().bounds.width
@@ -43,9 +43,11 @@ class DetailViewController: UIViewController {
         //通过数据流初始化图片
         let newImage = UIImage(data: urlData!)
         let imageView = UIImageView(image:newImage);
+        //imageView.frame.origin = CGPoint(x: 0, y: 0)
         imageView.frame = CGRectMake(0, 0, self.view.frame.size.width, 300)
         
-        //imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        
         //图片添加阴影(透明背景)
         //imageView.layer.shadowOpacity = 0.8
         //imageView.layer.shadowColor = UIColor.blackColor().CGColor
@@ -55,7 +57,7 @@ class DetailViewController: UIViewController {
         mainView.addSubview(imageView)
         
         let labelName: UILabel = UILabel()
-        labelName.frame = CGRect(x:5, y:300, width:self.view.frame.size.width-10, height:30)
+        labelName.frame = CGRect(x:5, y:imageView.frame.height, width:self.view.frame.size.width-10, height:30)
         labelName.text = data!.name
         labelName.backgroundColor = UIColor.cyanColor()
         labelName.adjustsFontSizeToFitWidth=true //自适应宽度
@@ -63,8 +65,16 @@ class DetailViewController: UIViewController {
         //labelName.shadowOffset=CGSizeMake(-2,2)  //阴影的偏移量
         mainView.addSubview(labelName)
         
+        let labelPrice: UILabel = UILabel()
+        labelPrice.frame = CGRect(x:5, y:imageView.frame.height+30, width:self.view.frame.size.width-10, height:30)
+        labelPrice.text = data!.price
+        labelPrice.backgroundColor = UIColor.orangeColor()
+        labelPrice.textColor=UIColor.redColor()
+        mainView.addSubview(labelPrice)
+        
+        
         let labelDetail: UILabel = UILabel()
-        labelDetail.frame = CGRect(x:5, y:360, width:self.view.frame.size.width-10, height:100)
+        labelDetail.frame = CGRect(x:5, y:imageView.frame.height+60, width:self.view.frame.size.width-10, height:100)
         labelDetail.text = data!.details
         labelDetail.backgroundColor = UIColor.whiteColor()
         labelDetail.textAlignment = NSTextAlignment.Left
@@ -73,18 +83,26 @@ class DetailViewController: UIViewController {
         labelDetail.adjustsFontSizeToFitWidth=true //自适应宽度
         labelDetail.numberOfLines=0     //多行显示
         mainView.addSubview(labelDetail)
+
         
-        let labelPrice: UILabel = UILabel()
-        labelPrice.frame = CGRect(x:5, y:330, width:self.view.frame.size.width-10, height:30)
-        labelPrice.text = data!.price
-        labelPrice.backgroundColor = UIColor.orangeColor()
-        labelPrice.textColor=UIColor.redColor()
-        mainView.addSubview(labelPrice)
+        let turl = "http://www.100bangai.co.jp/wp-content/themes/twentyfifteen/images/floor_map_ant.png"
+        
+        //定义NSURL对象
+        let url1 = NSURL(string: turl)
+        //从网络获取数据流
+        let urlData1 = NSData(contentsOfURL: url1!)
+        //通过数据流初始化图片
+        let newImage1 = UIImage(data: urlData1!)
+        let imageView1 = UIImageView(image:newImage1);
+        imageView1.frame = CGRectMake(50, imageView.frame.height+180, self.view.frame.size.width-100, 200)
+        
+        imageView1.contentMode = UIViewContentMode.ScaleAspectFit
+        mainView.addSubview(imageView1)
         
         //创建一个ContactAdd类型的按钮
         let button:UIButton = UIButton(type:.System)
         //设置按钮位置和大小
-        button.frame=CGRectMake(self.view.frame.width/2 - 100, 480, 200, 30)
+        button.frame=CGRectMake(self.view.frame.width/2 - 100, imageView.frame.height+400, 200, 30)
         //设置按钮文字
         button.backgroundColor = UIColor.groupTableViewBackgroundColor()
         button.setTitle("Add to shopCat", forState:UIControlState.Normal)
@@ -95,7 +113,6 @@ class DetailViewController: UIViewController {
         mainView.addSubview(button)
         
         self.view.addSubview(mainView)
-        
         
     }
     
